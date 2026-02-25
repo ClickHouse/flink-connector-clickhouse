bf6427ebc9842533e5b12d7d89c929134e9c4b63
Will Lauer <wlauer@aviatrix.com>
2026-02-24 23:04:28 -0600

    Adding flink 1.7 support

--- flink-connector-clickhouse-1.17/src/main/java/org/apache/flink/connector/clickhouse/sink/ClickHouseAsyncSink.java
+++ flink-connector-clickhouse-1.17/src/main/java/org/apache/flink/connector/clickhouse/sink/ClickHouseAsyncSink.java
@@ -2,15 +2,19 @@ package org.apache.flink.connector.clickhouse.sink;
 
 
 import com.clickhouse.data.ClickHouseFormat;
+import com.clickhouse.utils.writer.DataWriter;
+
 import org.apache.flink.api.connector.sink2.Sink;
 import org.apache.flink.connector.base.sink.AsyncSinkBase;
 import org.apache.flink.connector.base.sink.writer.BufferedRequestState;
 import org.apache.flink.connector.base.sink.writer.ElementConverter;
+import org.apache.flink.connector.clickhouse.convertor.SupportsHeader;
 import org.apache.flink.connector.clickhouse.data.ClickHousePayload;
 import org.apache.flink.core.io.SimpleVersionedSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
@@ -56,6 +60,21 @@ public class ClickHouseAsyncSink<InputT> extends AsyncSinkBase<InputT, ClickHous
 
     @Override
     public StatefulSinkWriter<InputT, BufferedRequestState<ClickHousePayload>> restoreWriter(Sink.InitContext context, Collection<BufferedRequestState<ClickHousePayload>> collection) throws IOException {
+        byte[] header = null;
+        if (clickHouseFormat != null && clickHouseFormat.hasHeader()) {
+            ElementConverter<InputT, ClickHousePayload> converter = getElementConverter();
+            if (converter == null || (!(converter instanceof SupportsHeader))) {
+                // unable to compute the header. Generate an error
+                throw new IllegalStateException("A converter that supports headers is required for the format " + clickHouseFormat.name());
+            }
+
+            SupportsHeader headerConvertor = (SupportsHeader) converter;
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            DataWriter dataWriter = DataWriter.of(true, out);
+            headerConvertor.header(dataWriter);
+            header = out.toByteArray();
+        }
+
         return new ClickHouseAsyncWriter<>(
                 getElementConverter(),
                 context,
@@ -67,6 +86,7 @@ public class ClickHouseAsyncSink<InputT> extends AsyncSinkBase<InputT, ClickHous
                 getMaxRecordSizeInBytes(),
                 clickHouseClientConfig,
                 clickHouseFormat,
+                header,
                 collection
         );
     }
